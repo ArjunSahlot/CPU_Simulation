490855988383d770fcd1fe421667b3ee0fddb6c2
Arjun <iarjun.sahlot@gmail.com>
2020-11-23 00:08:38 -0800

    Moving operators.

--- classes.py
+++ classes.py
@@ -1,2 +1,124 @@
 import pygame
-from CPU_Simulation.operations import *
+import CPU_Simulation.operations as ops
+from CPU_Simulation.constants import *
+
+pygame.init()
+
+COLOR_MAP = {
+    "and": (43, 124, 164),
+    "not": (146, 32, 25),
+    "nand": (93, 36, 180),
+    "or": (141, 78, 163),
+    "xor": (187, 58, 119),
+    "nor": (54, 116, 59),
+    "xnor": (129, 160, 200)
+}
+
+
+class Operator:
+    width, height = 200, 100
+    font = pygame.font.SysFont("comicsans", 45)
+
+    def __init__(self, x, y, name):
+        self.x, self.y = x, y
+        self.name = name.upper()
+        self.func = getattr(ops, "_" + name.lower())
+        self.color = COLOR_MAP[self.name.lower()]
+        if self.name.lower() == "not":
+            self.inputs = [Input(self, 1.5)]
+            self.height = 60
+        else:
+            self.inputs = [Input(self, 1), Input(self, 2)]
+        # self.output = Output()
+
+    def update(self, win, events):
+        mx, my = pygame.mouse.get_pos()
+        self.draw(win)
+        for input in self.inputs:
+            input.update(win)
+        # self.output.status = self.func(*self.inputs)
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.x < mx < self.x + self.width and self.y < my < self.y + self.height:
+                    return True
+            if event.type == pygame.MOUSEBUTTONUP:
+                return False
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        text = self.font.render(self.name, 1, WHITE)
+        win.blit(text, (self.x + self.width // 2 - text.get_width() // 2, self.y + self.height // 2 - text.get_height() // 3))
+
+    def __repr__(self):
+        return self.name
+
+
+class Input:
+    radius = 15
+    color = (0, 0, 0)
+
+    def __init__(self, host, index):
+        self.host = host
+        self.index = int((index - 1) * 2 + 1)
+        self.x = self.y = 0
+        self.output = None
+        self.status = None
+
+    def calc_pos(self):
+        self.x = self.host.x
+        self.y = int((self.host.height * (self.index / 4)) + self.host.y)
+
+    def update(self, win):
+        self.calc_pos()
+        if self.output is not None:
+            self.status = self.output.status
+        self.draw(win)
+
+    def mouse_release(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = event.pos
+                return (x - self.x)**2 + (y - self.y)**2 <= self.radius
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+    def plug(self, output):
+        self.output = output
+
+
+class Path:
+    width = 5
+
+    def __init__(self, input, output):
+        self.input, self.output = input, output
+        input.plug(output)
+
+
+class Text:
+    height = 65
+    y = 815
+    font = pygame.font.SysFont("comicsans", 45)
+
+    def __init__(self, x, text):
+        self.x = x
+        self.text = self.font.render(text.upper(), 1, WHITE)
+        self.width = self.text.get_width() + 30
+        self.color = (47, 47, 47)
+
+    def update(self, win, events):
+        self.draw(win)
+
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.Rect(self.x, self.y, self.width, self.height).collidepoint(pygame.mouse.get_pos()):
+                    self.color = (62, 62, 62)
+                    return True
+            if event.type == pygame.MOUSEBUTTONUP:
+                if not pygame.Rect(50, self.y, 704, self.height).collidepoint(pygame.mouse.get_pos()):
+                    self.color = (47, 47, 47)
+                    return False
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        win.blit(self.text, (self.x + self.width//2 - self.text.get_width()//2, self.y + self.height//2 - self.text.get_height()//3))
--- main.py
+++ main.py
@@ -1,21 +1,74 @@
 import pygame
 import os
 from CPU_Simulation.constants import *
+from CPU_Simulation.classes import *
 
+pygame.init()
 
 # Window Management
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("CPU Simulation")
 
 
+def draw_window(win):
+    win.fill((47, 47, 47))
+    pygame.draw.rect(win, (43, 43, 43), BOX, border_radius=10)
+    pygame.draw.rect(win, (60, 60, 60), BOX, 10, border_radius=10)
+
+
 def main(win, width, height):
     clock = pygame.time.Clock()
+    paths = []
+    ops = []
+    texts = {
+        "not": Text(50, "not"),
+        "and": Text(144, "and"),
+        "nand": Text(243, "nand"),
+        "or": Text(364, "or"),
+        "xor": Text(440, "xor"),
+        "nor": Text(536, "nor"),
+        "xnor": Text(634, "xnor"),
+    }
+
+    moving_ops = None
+    text_clicking = False
 
     run = True
     while run:
         clock.tick(FPS)
-        win.fill(BLACK)
+        draw_window(win)
         events = pygame.event.get()
+        mx, my = pygame.mouse.get_pos()
+
+        for name, text in texts.items():
+            state = text.update(win, events)
+            if state:
+                moving_ops = Operator(text.x, text.y, name)
+                ops.append(moving_ops)
+                text_clicking = True
+            elif state is not None and moving_ops is not None and text_clicking:
+                if BOX[0] < moving_ops.x and moving_ops.x + moving_ops.width < BOX[0] + BOX[2] and BOX[1] < moving_ops.y and moving_ops.y + moving_ops.height < BOX[1] + BOX[3]:
+                    ops.append(moving_ops)
+
+                moving_ops = None
+                text_clicking = False
+
+        for op in ops:
+            state = op.update(win, events)
+            if state:
+                ops.remove(op)
+                moving_ops = op
+                break
+            elif state is not None and moving_ops is not None:
+                if BOX[0] < moving_ops.x and moving_ops.x + moving_ops.width < BOX[0] + BOX[2] and BOX[1] < moving_ops.y and moving_ops.y + moving_ops.height < BOX[1] + BOX[3]:
+                    ops.append(moving_ops)
+
+                moving_ops = None
+
+        if moving_ops is not None:
+            moving_ops.update(win, events)
+            moving_ops.x, moving_ops.y = mx - moving_ops.width // 2, my - moving_ops.height // 2
+
         for event in events:
             if event.type == pygame.QUIT:
                 run = False
